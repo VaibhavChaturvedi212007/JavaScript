0ab8508ca523ebda1f532841ee8232201e18a3a8
VaibhavChaturvedi <chitranshchaturvedi21052007@gmail.com>
2026-01-20 09:33:02 +0530

    javascript variable

---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log("Hello");
+// console.log(`WORLD`);
+// window.alert("this is an alert box");
+// document.getElementById("myH1").textContent=`hello`;
+// document.getElementById("myP").textContent="this is my pizza";
+
+
+
+// VARIABLE: a container that stores a value.
+//           Behaves as if it were the value it contains.
+
+// 1.declarartion   letx;
+// 2.assignment    x=5;
+
+// let x;
+// x=100;
+// console.log(x);
+
+// let age=25;
+// let price=99.99;
+// console.log(`Your are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(typeof age);
+// console.log(typeof price);
+
+// let firstname="vaibhav";
+// let lastname="chaturvedi";
+// console.log(`my name is ${firstname} ${lastname}`);
+// console.log(typeof firstname);
+// console.log(typeof lastname);
+// let email="bro123@gmail.com";
+// console.log(`my email is ${email}`);
+// console.log(typeof email);
+
+// let online=true;
+// console.log(online);
+// console.log(typeof online);
+// let offline=false;
+// console.log(offline);
+// console.log(typeof offline);
+
+
+
+let fullname="vaibhav chaturvedi";
+let age=18;
+let student= false;
+
+document.getElementById("p1").textContent=fullname;
+document.getElementById("p2").textContent=age;
+document.getElementById("p3").textContent=student;
